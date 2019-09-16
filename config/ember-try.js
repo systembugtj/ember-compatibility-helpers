004495443402ea99f8c0548352fc56ea2a828db8
systembugtj <albert_lee@hotmail.com>
2019-09-16 16:36:26 -0700

    3.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,36 +1,72 @@
 'use strict';
 
-module.exports = function() {
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = async function() {
   return {
-    useYarn: true,
     scenarios: [
       {
-        name: 'ember-babel-6',
-        command: 'ember test',
+        name: 'ember-lts-3.4',
         npm: {
           devDependencies: {
-            'ember-cli-babel': '^6.16.0'
+            'ember-source': '~3.4.0'
           }
         }
       },
       {
-        name: 'node-babel-6',
-        command: 'mocha node-tests/babel-6/addon-test.js',
+        name: 'ember-lts-3.8',
         npm: {
           devDependencies: {
-            'ember-cli-babel': '^6.16.0'
+            'ember-source': '~3.8.0'
           }
         }
       },
       {
-        name: 'ember-babel-7',
-        command: 'ember test',
-        npm: {}
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release')
+          }
+        }
       },
       {
-        name: 'node-babel-7',
-        command: 'mocha node-tests/babel-7/addon-test.js',
-        npm: {}
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta')
+          }
+        }
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary')
+          }
+        }
+      },
+      // The default `.travis.yml` runs this scenario via `npm test`,
+      // not via `ember try`. It's still included here so that running
+      // `ember try:each` manually or from a customized CI config will run it
+      // along with all the other scenarios.
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {}
+        }
+      },
+      {
+        name: 'ember-default-with-jquery',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'jquery-integration': true
+          })
+        },
+        npm: {
+          devDependencies: {
+            '@ember/jquery': '^0.5.1'
+          }
+        }
       }
     ]
   };
